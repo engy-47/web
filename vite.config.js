b9d0d98cfb265a27eb451dd9d0414fd769af6600
Engy Emad <emadg9076@gmail.com>
2025-04-04 00:46:38 +0200

    Updated Version

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/engy-47.github.io/',
+  base: '/web/', 
   plugins: [react()],
 });
 
